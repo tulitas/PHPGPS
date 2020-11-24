30a12ccc503b1cfc2b4b286be5e9d335bc229c81
tulitas <tulitas1@gmail.com>
2020-11-24 16:34:22 +0200

    try to create markers

--- welcome.php
+++ welcome.php
@@ -5,24 +5,31 @@
     <div id="googleMap" style="width:100%;height:400px;"></div>
 
     <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
+    /** @var type $_POST */
+    $lat = $_POST['lat'];
+    $lng = $_POST['lng'];
+    
 ?>
     
 <script>  
+
 function myMap() {
-    let lat = '<?php echo $name;?>';
-    let lng = '<?php echo $email;?>';
-var mapProp= {   
-  center:new google.maps.LatLng('<?php echo $name;?>', '<?php echo $email;?>'),
-  zoom:5    
-};
-        var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    let myLatLng = { lat: lat, lng: lng};
-    let marker = new google.maps.Marker({position: myLatLng,  title: "hello"});
-    window.alert(lat + "\n " + lng);    
-    marker.setMap(map);
+   let lat = '<?php echo $lat;?>';
+   let lng = '<?php echo $lng;?>';
+   let myLatLng = new google.maps.LatLng(lat,lng);
+   var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    var mapProp= {
+    center: myLatLng,
+    zoom:6  
+     };
+   let marker =  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!"
+  });
+marker.setMap(map);
 }
+
 </script>
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBviR3u8gYcp3bLMEXIpqWmwCHj_5VYw_4&callback=myMap"></script>
 
